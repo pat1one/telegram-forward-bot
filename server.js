91368844a07eb62c2ffd7439822467c0c9474d7f
Platon <drow.battle.2015@gmail.com>
2025-07-28 05:27:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 const bot = new TelegramBot(process.env.BOT_TOKEN, { webHook: { port: PORT } });
 
-// Проверим, что все переменные заданы
+
 if (!process.env.BOT_TOKEN || !process.env.TO_USER_ID || !process.env.RENDER_EXTERNAL_URL) {
   throw new Error('Одна или несколько переменных окружения не заданы');
 }
 
-// Вебхук Telegram
+
 const WEBHOOK_PATH = `/bot${process.env.BOT_TOKEN}`;
 const WEBHOOK_URL = `${process.env.RENDER_EXTERNAL_URL}${WEBHOOK_PATH}`;
 
@@ -21,13 +21,13 @@ bot.setWebHook(WEBHOOK_URL)
   .then(() => console.log(`✅ Webhook установлен: ${WEBHOOK_URL}`))
   .catch((err) => console.error('❌ Ошибка установки webhook:', err.message));
 
-// Обработка входящих обновлений от Telegram
+
 app.post(WEBHOOK_PATH, (req, res) => {
   bot.processUpdate(req.body);
   res.sendStatus(200);
 });
 
-// Пересылка сообщений
+
 bot.on('message', async (msg) => {
   try {
     await bot.forwardMessage(process.env.TO_USER_ID, msg.chat.id, msg.message_id);
