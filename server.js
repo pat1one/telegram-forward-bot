11917f77bf4461617d337b4086590b5d52b738d8
Platon <drow.battle.2015@gmail.com>
2025-07-28 06:12:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -39,6 +39,10 @@ bot.on('message', async msg => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.send('🤖 Telegram бот работает! Webhook настроен.');
+});
+
 app.listen(PORT, () => {
   console.log(`🚀 Сервер запущен на порту ${PORT}`);
 });
